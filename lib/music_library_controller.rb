fe783e3f3dffb1a31990a7a22e5958be027642db
Chantel Lowe <chantellowe17@gmail.com>
2019-10-06 01:35:09 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,15 +26,18 @@ class MusicLibraryController
       input = gets.chomp
         
         case input
-          when "list songs"
-            list_songs
-          when "list artists"
-            list_artists
-          when "list genres"
-            list_genres
-          when ""
-          else
-            "Please enter a valid input."
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
         end
        
       end
